42c799ec7bdf89263f568761a741931e81fd377d
denny <denny.george90@gmail.com>
2020-10-27 19:42:35 +0530

    (chore)

--- src/api/api/gdrive-scraper/services/core/scraper.js
+++ src/api/api/gdrive-scraper/services/core/scraper.js
@@ -6,28 +6,31 @@ const {
   getFilesInTheseFolders,
   saveFilesFromGoogleDriveToDisk,
   parseWhatsappChatTextFileIntoJson,
-} = require("./gdrive");
-const { DateTime } = require("luxon");
+} = require("./gdrive")
+const { DateTime } = require("luxon")
 
-let changedWhatsappGroups;
-let report;
+let changedWhatsappGroups
+let report
 
+// async function GetStructuredDataFromGoogleDrive() {
+// var whatsappScraper = await getFolderFromDriveByName("Whatsapp-Scraper")
+// var modifiedWhatsappGroups = await getFoldersInThisFolder(whatsappScraper)
+// var folderAndFilesArray = await getFilesInTheseFolders(modifiedWhatsappGroups)
+// await saveFilesFromGoogleDriveToDisk(folderAndFilesArray)
+//
+// }
 getFolderFromDriveByName("Whatsapp-Scraper")
   .then(getFoldersInThisFolder)
   .then(getModifiedFoldersSinceLastScrapeTime)
-  .then((modifiedWhatsappGroups) => {
-    console.log(
-      `Modified Folders : ${modifiedWhatsappGroups
-        .map((group) => group.name)
-        .join(", ")}`
-    );
-    changedWhatsappGroups = modifiedWhatsappGroups;
-    return getFilesInTheseFolders(modifiedWhatsappGroups);
+  .then(modifiedWhatsappGroups => {
+    console.log(`Modified Folders : ${modifiedWhatsappGroups.map(group => group.name).join(", ")}`)
+    changedWhatsappGroups = modifiedWhatsappGroups
+    return getFilesInTheseFolders(modifiedWhatsappGroups)
   })
   .then(saveFilesFromGoogleDriveToDisk)
-  .then((downloadAllFiles) => Promise.all(downloadAllFiles))
+  .then(downloadAllFiles => Promise.all(downloadAllFiles))
   .then(() => parseWhatsappChatTextFileIntoJson(changedWhatsappGroups))
   .then(() => modifyLastUpdatedTimeInRecord(changedWhatsappGroups))
-  .then((res) => console.log("res", res))
-  .then((res) => console.log("done"))
-  .catch((err) => console.log(err));
+  .then(res => console.log("res", res))
+  .then(res => console.log("done"))
+  .catch(err => console.log(err))
